37cd7ac8c230afb3e14c4514b02253f9f86f1aa0
Полевой Никита Романович <npolevoy@kplatform.ru>
2017-12-06 16:09:31 +0300

    *remote processing

--- run_tpl.bat
+++ run_tpl.bat
@@ -1,3 +1,12 @@
 @echo off
-node index . && grunt && pause
-pause
+
+set OLDDIR=%CD%
+set OLDDRIVE=%~d0
+
+%systemdrive%
+cd %appdata%/tiny-tpl-engine
+
+rd temp /s /q
+md temp
+
+node %appdata%/tiny-tpl-engine/index.js %OLDDIR% && grunt default --path=%OLDDIR% & %OLDDRIVE% & cd %OLDDIR% & pause
