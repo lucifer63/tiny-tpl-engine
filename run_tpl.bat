f95b9921fca0e2ce2d22835da969f1a278e5a6d1
Полевой Никита Романович <npolevoy@kplatform.ru>
2017-12-27 13:14:44 +0300

    *run_tpl.bat

--- run_tpl.bat
+++ run_tpl.bat
@@ -6,4 +6,4 @@ set OLDDRIVE=%~d0
 %systemdrive%
 cd %appdata%/tiny-tpl-engine
 
-node %appdata%/tiny-tpl-engine/index.js "%OLDDIR%" && grunt default --path="%OLDDIR%" & %OLDDRIVE% & cd "%OLDDIR%" & pause
+node index.js "%OLDDIR%" && grunt default --path="%OLDDIR%" & %OLDDRIVE% & cd "%OLDDIR%" & pause
